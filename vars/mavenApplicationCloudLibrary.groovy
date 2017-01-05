64074b1e53acb6b2c0ed99d9d4fa6ec695fd220e
Kristóf Bodnár <Kristof.Bodnar@wup.hu>
2017-01-05 18:29:56 +0100

    create vars folder and a groovy script

---
+++ vars/mavenApplicationCloudLibrary.groovy
@@ -0,0 +1,30 @@
+def call(body):
+	 // evaluate the body block, and collect configuration into the object
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+	node{
+		stage('Checkout'){
+			checkout scm
+		}
+		
+		stage('Build'){
+			withEnv(["PATH+MAVEN=${tool 'M3'}"]){
+				sh 'mvn clean install'
+			}
+		}
+		
+		stage('Deploy to Cloud Foundry'){
+			sh '''
+				password=`cat pw`
+				cf login -a "${config.apiEndPoint}" -o "devops-app-test" -s "development" -u "matrixsprt@gmail.com" -p "$password"
+				cf push word-gather -m 512M -p target/backend-template-0.0.1-SNAPSHOT.jar
+				cf start word-gather
+			   '''
+		}
+		
+		stage('Application Status'){
+			sh 'cf apps'
+		}
+	}
