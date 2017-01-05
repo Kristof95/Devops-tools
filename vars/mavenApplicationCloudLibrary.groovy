d5c6a029451a3597867f6b1ca6064bcdcf337329
Kristóf Bodnár <Kristof.Bodnar@wup.hu>
2017-01-05 20:14:46 +0100

    remove node from groovy script

--- vars/mavenApplicationCloudLibrary.groovy
+++ vars/mavenApplicationCloudLibrary.groovy
@@ -5,28 +5,27 @@ def call(body){
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
     body()
-	node{
-		stage('Checkout'){
-			checkout scm
+	
+	stage('Checkout'){
+		checkout scm
+	}
+	
+	stage('Build'){
+		withEnv(["PATH+MAVEN=${tool 'M3'}"]){
+			sh 'mvn clean install'
 		}
-		
-		stage('Build'){
-			withEnv(["PATH+MAVEN=${tool 'M3'}"]){
-				sh 'mvn clean install'
-			}
-		}
-		
-		stage('Deploy to Cloud Foundry'){
-			sh '''
-				password=`cat pw`
-				cf login -a "${config.apiEndPoint}" -o "devops-app-test" -s "development" -u "matrixsprt@gmail.com" -p "$password"
-				cf push word-gather -m 512M -p target/backend-template-0.0.1-SNAPSHOT.jar
-				cf start word-gather
-			   '''
-		}
-		
-		stage('Application Status'){
-			sh 'cf apps'
-		}
+	}
+	
+	stage('Deploy to Cloud Foundry'){
+		sh '''
+			password=`cat pw`
+			cf login -a "${config.apiEndPoint}" -o "devops-app-test" -s "development" -u "matrixsprt@gmail.com" -p "$password"
+			cf push word-gather -m 512M -p target/backend-template-0.0.1-SNAPSHOT.jar
+			cf start word-gather
+		   '''
+	}
+	
+	stage('Application Status'){
+		sh 'cf apps'
 	}
 }
