2b4860032042afdadd41e8f5e57ed917e483ab24
Kristof95 <matrixsprt@gmail.com>
2017-02-03 16:12:54 +0100

    add body

--- vars/mavenApplicationCloudLibrary.groovy
+++ vars/mavenApplicationCloudLibrary.groovy
@@ -1,5 +1,12 @@
 node{
-
+    def call(body) {
+    // evaluate the body block, and collect configuration into the object
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+	
+	
 	stage('Hello'){
 		echo "master"
 	}
