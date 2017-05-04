d775984d869ea8c564d56a3210b11b83ddcd2053
Kristóf Bodnár <Kristof.Bodnar@wup.hu>
2017-05-04 16:31:54 +0200

    test

--- vars/mavenApplicationCloudLibrary.groovy
+++ vars/mavenApplicationCloudLibrary.groovy
@@ -1,22 +1,4 @@
 
-static void addSharedLibraryBranch(String branch) {
-    def issueNumber = branch.split("/")[1]
-    def sharedLibBranch = """@Library("pipeline-shared-lib@$branch")"""
-    def jenkinsFile = readFile "Jenkinsfile"
-    def newContent = jenkinsFile.replace(jenkinsFile, "$sharedLibBranch\n$jenkinsFile")
-    def credential = readFile 'cred'
-    def pw = credential.split("/")[1]
-    echo "Content: $newContent"
-    writeFile file: "Jenkinsfile", text: "$newContent"
-    sh """
-        git config remote.origin.url https://Kristof95:$pw@github.com/Kristof95/devops_test_repo.git
-        git checkout "$branch"
-        git add .
-        git commit -m "$issueNumber set shared library branch in Jenkinsfile"
-        git push
-    """
-}
-
 def call(body) {
 // evaluate the body block, and collect configuration into the object
     def config = [:]
@@ -38,4 +20,22 @@ def call(body) {
             echo "Skipping shared library branch settings!"
         }
     }
+}
+
+static void addSharedLibraryBranch(String branch) {
+    def issueNumber = branch.split("/")[1]
+    def sharedLibBranch = """@Library("pipeline-shared-lib@$branch")"""
+    def jenkinsFile = readFile "Jenkinsfile"
+    def newContent = jenkinsFile.replace(jenkinsFile, "$sharedLibBranch\n$jenkinsFile")
+    def credential = readFile '/var/jenkins_home/workspace/cred'
+    def pw = credential.split(":")[1]
+    echo "Content: $newContent"
+    writeFile file: "Jenkinsfile", text: "$newContent"
+    sh """
+        git config remote.origin.url https://Kristof95:$pw@github.com/Kristof95/devops_test_repo.git
+        git checkout "$branch"
+        git add .
+        git commit -m "$issueNumber set shared library branch in Jenkinsfile"
+        git push
+    """
 }
