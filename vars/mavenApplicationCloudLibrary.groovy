02c3abc91f1898cb0f6b11a4f5db3ee63674c3f8
Kristóf Bodnár <Kristof.Bodnar@wup.hu>
2017-05-04 15:05:55 +0200

    asdas

--- vars/mavenApplicationCloudLibrary.groovy
+++ vars/mavenApplicationCloudLibrary.groovy
@@ -1,4 +1,21 @@
-import util.SharedLibBranchUtility
+
+static void addSharedLibraryBranch(String branch) {
+    def issueNumber = branch.split("/")[1]
+    def sharedLibBranch = """@Library("pipeline-shared-lib@$branch")"""
+    def jenkinsFile = readFile "Jenkinsfile"
+    def newContent = jenkinsFile.replace(jenkinsFile, "$sharedLibBranch\n$jenkinsFile")
+    def credential = readFile 'cred'
+    def pw = credential.split("/")[1]
+    echo "Content: $newContent"
+    writeFile file: "Jenkinsfile", text: "$newContent"
+    sh """
+        git config remote.origin.url https://Kristof95:$pw@github.com/Kristof95/devops_test_repo.git
+        git checkout "$branch"
+        git add .
+        git commit -m "$issueNumber set shared library branch in Jenkinsfile"
+        git push
+    """
+}
 
 def call(body) {
 // evaluate the body block, and collect configuration into the object
@@ -6,6 +23,7 @@ def call(body) {
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
     body()
+
     node {
 
         stage('Hello') {
@@ -15,7 +33,7 @@ def call(body) {
         stage('Set shared library branch') {
             def branch = "$env.BRANCH_NAME"
             if (branch ==~ /^(feature).+/) {
-                SharedLibBranchUtility.addSharedLibraryBranch(branch)
+                addSharedLibraryBranch(branch)
             }
             echo "Skipping shared library branch settings!"
         }
