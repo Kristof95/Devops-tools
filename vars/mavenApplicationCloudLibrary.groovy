99493cf2ca4c646811b121e5ff1ab982568f2935
Kristóf Bodnár <Kristof.Bodnar@wup.hu>
2017-05-04 14:59:40 +0200

    test new things

--- vars/mavenApplicationCloudLibrary.groovy
+++ vars/mavenApplicationCloudLibrary.groovy
@@ -1,1 +1,23 @@
+import util.SharedLibBranchUtility
 
+def call(body) {
+// evaluate the body block, and collect configuration into the object
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+    node {
+
+        stage('Hello') {
+            echo "Hy from master branch"
+        }
+
+        stage('Set shared library branch') {
+            def branch = "$env.BRANCH_NAME"
+            if (branch ==~ /^(feature).+/) {
+                SharedLibBranchUtility.addSharedLibraryBranch(branch)
+            }
+            echo "Skipping shared library branch settings!"
+        }
+    }
+}
