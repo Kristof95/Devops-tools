5e6609d590fe5ec86d3d63157879f277e9baa766
Kristof95 <matrixsprt@gmail.com>
2017-05-04 16:51:13 +0200

    Update mavenApplicationCloudLibrary.groovy

--- vars/mavenApplicationCloudLibrary.groovy
+++ vars/mavenApplicationCloudLibrary.groovy
@@ -15,27 +15,23 @@ def call(body) {
         stage('Set shared library branch') {
             def branch = "$env.BRANCH_NAME"
             if (branch ==~ /^(feature).+/) {
-                addSharedLibraryBranch(branch)
+                def issueNumber = branch.split("/")[1]
+                def sharedLibBranch = """@Library("pipeline-shared-lib@$branch") _"""
+                def jenkinsFile = readFile "Jenkinsfile"
+                def newContent = jenkinsFile.replace(jenkinsFile, "$sharedLibBranch\n$jenkinsFile")
+                def credential = readFile '/var/jenkins_home/workspace/cred'
+                def pw = credential.split(":")[1]
+                echo "Content: $newContent"
+                writeFile file: "Jenkinsfile", text: "$newContent"
+                sh """
+                    git config remote.origin.url https://Kristof95:$pw@github.com/Kristof95/devops_test_repo.git
+                    git checkout "$branch"
+                    git add .
+                    git commit -m "$issueNumber set shared library branch in Jenkinsfile"
+                    git push
+                """
             }
             echo "Skipping shared library branch settings!"
         }
     }
-}
-
-static void addSharedLibraryBranch(String branch) {
-    def issueNumber = branch.split("/")[1]
-    def sharedLibBranch = """@Library("pipeline-shared-lib@$branch") _"""
-    def jenkinsFile = readFile "Jenkinsfile"
-    def newContent = jenkinsFile.replace(jenkinsFile, "$sharedLibBranch\n$jenkinsFile")
-    def credential = readFile '/var/jenkins_home/workspace/cred'
-    def pw = credential.split(":")[1]
-    echo "Content: $newContent"
-    writeFile file: "Jenkinsfile", text: "$newContent"
-    sh """
-        git config remote.origin.url https://Kristof95:$pw@github.com/Kristof95/devops_test_repo.git
-        git checkout "$branch"
-        git add .
-        git commit -m "$issueNumber set shared library branch in Jenkinsfile"
-        git push
-    """
 }
